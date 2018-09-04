2ddca03c456b460b6e4889903b3935676722cc2f
mht <952326644@qq.com>
2018-09-05 00:33:09 +0800

    Base64编解码

--- src/Java8_New_Features/Base64/Base64Demo.java
+++ src/Java8_New_Features/Base64/Base64Demo.java
@@ -6,13 +6,12 @@ import java.util.Base64;
 public class Base64Demo {
     public static void main(String[] args) {
         try {
-            String encodedUrl = Base64.getUrlEncoder().encodeToString("/test/base64?name=威尔·史密斯".getBytes("UTF-8"));
-            System.out.println(encodedUrl);
-            String decodedUrl = new String(Base64.getUrlDecoder().decode(encodedUrl));
-            System.out.println(decodedUrl);
+            String encoded = Base64.getEncoder().encodeToString("Will Smith = 威尔·史密斯".getBytes("UTF-8"));
+            System.out.println(encoded);
+            String decoded = new String(Base64.getDecoder().decode(encoded));
+            System.out.println(decoded);
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
         }
     }
-
 }
