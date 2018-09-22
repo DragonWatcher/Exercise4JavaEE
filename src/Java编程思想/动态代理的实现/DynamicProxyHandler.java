0e03f1d10f45138d50b92ce85e4941dab3a7c363
mht <952326644@qq.com>
2018-09-22 09:56:45 +0800

    代码优化

--- src/Java编程思想/动态代理的实现/DynamicProxyHandler.java
+++ src/Java编程思想/动态代理的实现/DynamicProxyHandler.java
@@ -3,12 +3,19 @@ package Java编程思想.动态代理的实现;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 
+/**
+ * 动态代理器
+ * 用于将
+ * <br>类名：DynamicProxyHandler<br>
+ * 作者： mht<br>
+ * 日期： 2018年9月22日-上午9:03:09<br>
+ */
 public class DynamicProxyHandler implements InvocationHandler{
 
-    private Object proxied;
+    private Object real;
     
-    public DynamicProxyHandler(Object proxied) {
-        this.proxied = proxied;
+    public DynamicProxyHandler(Object real) {
+        this.real = real;
     }
 
     @Override
@@ -21,6 +28,6 @@ public class DynamicProxyHandler implements InvocationHandler{
             }
         }
         
-        return method.invoke(proxied, args);
+        return method.invoke(real, args);
     }
 }
--- src/Java编程思想/动态代理的实现/SimpleDynamicProxy.java
+++ src/Java编程思想/动态代理的实现/SimpleDynamicProxy.java
@@ -10,9 +10,11 @@ public class SimpleDynamicProxy {
     }
 
     public static void main(String[] args) {
+        // 无代理时使用真实对象
         RealObject real = new RealObject();
         consumer(real);
-
+        System.out.println("----------");
+        // 创建一个同样继承自封装接口的代理对象，这就意味着代理对象可以表现出和真实对象相同的行为
         Interface proxy = (Interface) Proxy.newProxyInstance(Interface.class.getClassLoader(),
                 new Class[] { Interface.class }, new DynamicProxyHandler(real));
 
