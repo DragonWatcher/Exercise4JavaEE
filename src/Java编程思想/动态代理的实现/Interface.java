2494c17f0cbb6794d88cd069d318c9840a92501a
mht <952326644@qq.com>
2018-09-22 08:47:11 +0800

    注释

--- src/Java编程思想/动态代理的实现/Interface.java
+++ src/Java编程思想/动态代理的实现/Interface.java
@@ -1,5 +1,10 @@
 package Java编程思想.动态代理的实现;
-
+/**
+ * 代理接口
+ * <br>类名：Interface<br>
+ * 作者： mht<br>
+ * 日期： 2018年9月22日-上午8:44:28<br>
+ */
 interface Interface {
     
     void doSomething();
--- src/Java编程思想/动态代理的实现/RealObject.java
+++ src/Java编程思想/动态代理的实现/RealObject.java
@@ -1,5 +1,10 @@
 package Java编程思想.动态代理的实现;
-
+/**
+ * 实际的需要被代理的对象
+ * <br>类名：RealObject<br>
+ * 作者： mht<br>
+ * 日期： 2018年9月22日-上午8:44:05<br>
+ */
 class RealObject implements Interface{
 
     @Override
