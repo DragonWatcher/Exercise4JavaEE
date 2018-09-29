e5311b9fcc23b426db3a3522e53a326d8b3721e2
mht <952326644@qq.com>
2018-09-29 11:00:15 +0800

    线程不论是否获得锁，都将被interrupt()方法打断

--- src/马士兵高并发编程公开课/ReentrantLock/InterruptWaitingDemo.java
+++ src/马士兵高并发编程公开课/ReentrantLock/InterruptWaitingDemo.java
@@ -5,7 +5,7 @@ import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 /**
- * 线程打断等待 使用ReentrantLock还可以调用lockInterruptibly方法，可以对线程interrupt方法做出响应。
+ * 线程打断等待 使用ReentrantLock还可以调用lockInterruptibly方法，可以对线程的interrupt方法做出响应。
  * 在一个线程等待锁的过程中，可以被打断。 <br>
  * 类名：InterruptWaitingDemo<br>
  * 作者： mht<br>
@@ -39,7 +39,7 @@ public class InterruptWaitingDemo {
             } catch (InterruptedException e) {
                 System.out.println("Interrupted!");
             } finally {
-                System.out.println("lock = " + lock.tryLock());
+                System.out.println("finally：lock = " + lock.tryLock());
                 lock.unlock();
             }
         }, "t2");
@@ -51,7 +51,7 @@ public class InterruptWaitingDemo {
             e.printStackTrace();
         }
 
-        t2.interrupt();// 打断线程 t2 的等待。
+        t2.interrupt();// 打断线程 t2，不论线程是否获得锁。
     }
 
 }
