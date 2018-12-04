ce1fe6d1b748e7cc1a9b01dd6b19875cac105e5b
mht <952326644@qq.com>
2018-12-04 15:32:38 +0800

    项目转换为maven项目

---
+++ src/XML学习/XMLParser.java
@@ -0,0 +1,17 @@
+package XML学习;
+
+import org.dom4j.io.SAXReader;
+
+/**
+ * 解析自定义xml文件
+ * @author mht
+ * @date 2018年12月4日下午3:22:26
+ */
+public class XMLParser {
+	
+	public void parseXML() {
+		SAXReader saxRdr = new SAXReader();
+		
+	}
+
+}
