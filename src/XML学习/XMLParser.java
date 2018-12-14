1d48e8a92b0fa158e1913057c863ed41c2c00e83
mht <952326644@qq.com>
2018-12-14 21:43:29 +0800

    xml学习

--- src/XML学习/XMLParser.java
+++ src/XML学习/XMLParser.java
@@ -1,6 +1,13 @@
 package XML学习;
 
+import java.io.File;
+import java.io.UnsupportedEncodingException;
+
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Element;
 import org.dom4j.io.SAXReader;
+import org.junit.Test;
 
 /**
  * 解析自定义xml文件
@@ -9,8 +16,19 @@ import org.dom4j.io.SAXReader;
  */
 public class XMLParser {
 	
-	public void parseXML() {
-		SAXReader saxRdr = new SAXReader();
+	@Test
+	public void parseXML() throws UnsupportedEncodingException {
+		
+		try {
+			SAXReader saxRdr = new SAXReader();
+			Document xmlDom = saxRdr.read(new File(this.getClass().getResource("").toString().getBytes("UTF-8").toString() + "/demo.xml"));
+			Element rootEle = xmlDom.getRootElement();
+			
+			System.out.println(rootEle.toString());
+			System.out.println(rootEle.getName());
+		} catch (DocumentException e) {
+			e.printStackTrace();
+		}
 		
 	}
 
